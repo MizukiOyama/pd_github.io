93aa3563623e809384ff6f2120eb2f4e7dab99ac
MizukiOyama <154662763+MizukiOyama@users.noreply.github.com>
2024-08-01 21:27:28 +0900

    head

--- js/menu.js
+++ js/menu.js
@@ -107,7 +107,7 @@ function initializeMenu() {
 
 // ヘッダーの読み込みと初期化
 document.addEventListener("DOMContentLoaded", function() {
-    fetch("includes-header.html")
+    fetch("header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header-container").innerHTML = data;
