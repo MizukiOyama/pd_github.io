e857038d44f07ab2aa2c8783805d2c6e217be2a2
MizukiOyama <154662763+MizukiOyama@users.noreply.github.com>
2024-08-03 11:43:54 +0900

    foot js tab colo hei

--- js/template-foot.js
+++ js/template-foot.js
@@ -1,13 +1,5 @@
 // ヘッダーとフッターの読み込みと初期化
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("head.html")
-        .then(response => response.text())
-        .then(headerData => {
-            document.getElementById("head-container").innerHTML = headerData;
-            // メニューの初期化を再度呼び出し
-            initializeMenu();
-        });
-
     fetch("footer.html")
         .then(response => response.text())
         .then(footerData => {
