ce7d9dbebde37da9ad40c49a093f0ea69869dc0e
MizukiOyama <154662763+MizukiOyama@users.noreply.github.com>
2024-08-05 11:13:07 +0900

    form menu js

--- js/menu.js
+++ js/menu.js
@@ -72,4 +72,19 @@ document.addEventListener("DOMContentLoaded", function() {
             img.removeAttribute('data-src');
         };
     });
+
+    // ヘッダーとフッターの読み込みと初期化
+    fetch("header.html")
+        .then(response => response.text())
+        .then(headerData => {
+            document.getElementById("header-container").innerHTML = headerData;
+            // メニューの初期化を再度呼び出し
+            initializeMenu();
+        });
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(footerData => {
+            document.getElementById("footer-container").innerHTML = footerData;
+        });
 });
